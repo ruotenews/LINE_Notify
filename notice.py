aea7cbf1b73e50672fae183057c9ce2fb168851e
nanato12 <admin@nanato12.info>
2020-10-29 10:42:03 +0900

    Rename test.py -> notice.py

--- notice.py
+++ notice.py
@@ -7,6 +7,7 @@ load_dotenv(verbose=True)
 load_dotenv('.env')
 
 DB_FILE_PATH = os.environ.get("DB_FILE_PATH")
+NOTIFY_TOKEN = os.environ.get("NOTIFY_TOKEN")
 
-notify_news = NotifyNews(DB_FILE_PATH)
+notify_news = NotifyNews(DB_FILE_PATH, NOTIFY_TOKEN)
 notify_news.refresh()
